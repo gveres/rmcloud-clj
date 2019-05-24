2d9399159f9e43b50dfc9c3748a9b9a70ad4c8d2
Gabor Veres <gabor.veres@gmail.com>
2019-05-24 13:21:31 +0200

    Implement notification api

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [clj-http "3.9.1"]
                  [stylefruits/gniazdo "1.1.1"]
                  [org.clojure/tools.logging "0.4.1"]
--- src/rmcloud_clj/ws_listener.clj
+++ src/rmcloud_clj/ws_listener.clj
@@ -1,3 +1,18 @@
 (ns rmcloud-clj.ws-listener
-  (:require [rmcloud-clj.client-state :refer [config]]))
+  (:require [rmcloud-clj.client-state :refer [api-context]]
+            [gniazdo.core :as ws]
+            [clojure.data.json :as json]))
 
+(defn- process-message [on-receive message]
+  (let [edned (json/read-str message :key-fn keyword)]
+    (on-receive edned)))
+
+(defn subscribe-notifications [on-receive]
+ (let [ws-url (rmcloud-clj.core/service-discovery "notifications" 1)]
+   (ws/connect
+    "wss://5zt5-notifications-production.cloud.remarkable.engineering/notifications/ws/json/1"
+    :headers {"Authorization" (str "Bearer " (rmcloud-clj.core/refresh-token (rmcloud-clj.client-state/get-token-from-dotfile)))}
+    :on-receive #(process-message on-receive %))))
+
+(defn unsubscribe-notifications [notif-channel]
+  (ws/close notif-channel))
